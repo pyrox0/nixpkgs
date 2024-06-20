96c41e16cbb11483e1f57274775e71f963e633bc
Pyrox <pyrox@pyrox.dev>
2024-06-19 20:37:31 -0400

    cargo-binstall: 1.6.9 -> 1.7.1

--- pkgs/development/tools/rust/cargo-binstall/default.nix
+++ pkgs/development/tools/rust/cargo-binstall/default.nix
@@ -11,16 +11,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-binstall";
-  version = "1.6.9";
+  version = "1.7.1";
 
   src = fetchFromGitHub {
     owner = "cargo-bins";
     repo = "cargo-binstall";
     rev = "v${version}";
-    hash = "sha256-gByMCbh/RswZFrGY176PcZbBeHRsD0mv8Om4xQ/1wTo=";
+    hash = "sha256-zIPo7VqW5CdjL+CUc0TUVfYjLVWpyPg0Jd859xJPWNo=";
   };
 
-  cargoHash = "sha256-GPDDKFM2dxsjQcrNK7y2lydnqGnFBUMcDJZ420jmzlo=";
+  cargoHash = "sha256-pgoXAy8PR3xObhz8ntFyIvKgXdd708GaiNLF5mORnHI=";
 
   nativeBuildInputs = [
     pkg-config
