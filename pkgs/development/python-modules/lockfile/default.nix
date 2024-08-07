76cfe5c0a9337bca4b152a2cae47c6c3e3f92501
Pyrox <pyrox@pyrox.dev>
2024-08-07 14:36:04 -0400

    python312Packages.lockfile: modernize

--- pkgs/development/python-modules/lockfile/default.nix
+++ pkgs/development/python-modules/lockfile/default.nix
@@ -14,9 +14,11 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "6aed02de03cba24efabcd600b30540140634fc06cfa603822d508d5361e9f799";
+    hash = "sha256-au0C3gPLok76vNYAswVAFAY0/AbPpgOCLVCNU2Hp95k=";
   };
 
+  # The __init__ method doesn't do anything relevant as far as I can tell
+  # So we disable it, since it also makes pytest fail.
   postPatch = ''
     substituteInPlace test/compliancetest.py \
       --replace-fail "def setup" "def setUp" \
@@ -31,9 +33,9 @@ buildPythonPackage rec {
 
   nativeCheckInputs = [ pytestCheckHook ];
 
-  meta = with lib; {
+  meta = {
     homepage = "https://launchpad.net/pylockfile";
     description = "Platform-independent advisory file locking capability for Python applications";
-    license = licenses.asl20;
+    license = lib.licenses.asl20;
   };
 }
